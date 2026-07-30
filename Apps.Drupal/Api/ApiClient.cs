3923adcb1a7da0721ed08065fb1a7bc92ea44053
vitalii-bezuhlyi <vitalii.bezuhlyi@blackbird.io>
2026-07-30 15:23:49 +0300

    Added retry policy

--- Apps.Drupal/Api/ApiClient.cs
+++ Apps.Drupal/Api/ApiClient.cs
@@ -1,3 +1,4 @@
+using System.Net;
 using System.Text.RegularExpressions;
 using System.Web;
 using Apps.Drupal.Constants;
@@ -5,6 +6,8 @@ using Blackbird.Applications.Sdk.Common.Authentication;
 using Blackbird.Applications.Sdk.Common.Exceptions;
 using Blackbird.Applications.Sdk.Utils.Extensions.Sdk;
 using Blackbird.Applications.Sdk.Utils.RestSharp;
+using Polly;
+using Polly.Retry;
 using RestSharp;
 
 namespace Apps.Drupal.Api;
@@ -14,6 +17,24 @@ public class ApiClient(IEnumerable<AuthenticationCredentialsProvider> credential
     BaseUrl = new Uri(credentials.Get(CredsNames.BaseUrl).Value)
 })
 {
+    private const int RetryCount = 3;
+    private static readonly Random Jitter = new();
+
+    private readonly AsyncRetryPolicy<RestResponse> _retryPolicy = Policy
+        .HandleResult<RestResponse>(r => r.StatusCode == HttpStatusCode.GatewayTimeout || r.StatusCode == HttpStatusCode.TooManyRequests)
+        .WaitAndRetryAsync(RetryCount, attempt =>
+            TimeSpan.FromSeconds(Math.Pow(2, attempt)) + TimeSpan.FromMilliseconds(Jitter.Next(0, 500)),
+            (_, _, _, _) => Task.CompletedTask);
+
+    public override async Task<RestResponse> ExecuteWithErrorHandling(RestRequest request)
+    {
+        var response = await _retryPolicy.ExecuteAsync(() => ExecuteAsync(request));
+        if (!response.IsSuccessStatusCode)
+            ConfigureErrorException(response);
+
+        return response;
+    }
+
     protected override Exception ConfigureErrorException(RestResponse response)
     {
         var errorMessage = $"Status code: {(int)response.StatusCode}";
